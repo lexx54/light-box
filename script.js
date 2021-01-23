a11d14c4453e5fdd69fae5f2a7cdab79a65a7550
lexx54 <chapelexx54@gmail.com>
2021-01-22 20:14:29 -0400

    aplied some jquery to display the photos

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 // document.querySelector(".photo").style.display="none";
-$(".photo img").hover(function(){
-  console.log(this);
+$(".photo img").mouseenter(function(){
+  $('aside').css('z-index','2').css('opacity','1');
+  $('aside img').attr('src',this.src);
+});
+$(".photo img").mouseout(function(){
+  $('aside').css('z-index','-1').css('opacity','0');
 });
