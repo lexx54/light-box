a6368e4c3035d7a541e9f7d5de94213235b4495a
lexx54 <chapelexx54@gmail.com>
2021-02-01 19:52:48 -0400

    hide the description of the photos and displayed in the carrousel

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
 // document.querySelector(".photo").style.display="none";
+$("section .row:nth-child(2) div span").hide();
 $(".photo img").click(function(){
   $('aside').css('z-index','3').css('opacity','1');
   $('aside img').attr('src',this.src);
+
+ let $text=document.createElement("p");
+  $text.textContent=this.parentNode.children[1].textContent;
+  $text.className="carrou-text";
+
+  $('aside p').append($text);
+ 
+
+  // console.log($text)
+  // $('aside').append($text)
 });
 
 $(".x").click(function(){
   $('aside').css('z-index','-1').css('opacity','0');
+  $('aside p').empty();
 });
