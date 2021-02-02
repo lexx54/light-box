f0bebb01d0df196dd4c61a9d1604954fcfb653a8
lexx54 <chapelexx54@gmail.com>
2021-02-01 20:01:05 -0400

    fixed the displayment of text in the carrousel

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 // document.querySelector(".photo").style.display="none";
 $("section .row:nth-child(2) div span").hide();
 $(".photo img").click(function(){
-  $('aside').css('z-index','3').css('opacity','1');
-  $('aside img').attr('src',this.src);
-
- let $text=document.createElement("p");
+  let $text=document.createElement("p");
   $text.textContent=this.parentNode.children[1].textContent;
   $text.className="carrou-text";
 
-  $('aside p').append($text);
- 
+  $('aside').css('z-index','3').css('opacity','1');
+  $('aside img').attr('src',this.src);
 
-  // console.log($text)
-  // $('aside').append($text)
+  $('aside p').has("p")? (
+    $('aside p').empty(),
+    $('aside p').append($text)
+  ):$('aside p').append($text);
 });
 
 $(".x").click(function(){
